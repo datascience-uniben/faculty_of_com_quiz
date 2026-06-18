b4431cd2cc5090819d2bfac1e28a74b8325f64c1
datascience-uniben <datascience@uniben.edu>
2026-06-18 04:22:09 +0100

    Update quizmastergamma.py

--- quizmastergamma.py
+++ quizmastergamma.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # -------------------------------
 st.set_page_config(
     page_title="Faculty of Computing Quiz Competition",
-    page_icon="🏆",
+    page_icon="logo.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
